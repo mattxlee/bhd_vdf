4b543d2cc4fd169d630998eeb461851455603123
Matthew Lee <mattxlee@gmail.com>
2022-03-10 20:40:35 +0800

    Format code for max line length 80.

--- src/vdf_server.cpp
+++ src/vdf_server.cpp
@@ -1,9 +1,8 @@
 #include <array>
+#include <boost/asio.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
-
-#include <boost/asio.hpp>
 namespace asio = boost::asio;
 using namespace asio::ip;
 using asio::detail::socket_ops::host_to_network_short;
@@ -24,29 +23,30 @@ using Message = google::protobuf::Message;
 class MessageFactory
 {
 public:
-    virtual ~MessageFactory() { }
+  virtual ~MessageFactory() {}
 
-    virtual int get_msg_id() const = 0;
+  virtual int get_msg_id() const = 0;
 
-    virtual Message* parse(uint8_t const* data, int& size) const = 0;
+  virtual Message* parse(uint8_t const* data, int& size) const = 0;
 };
 
-template <int ID, typename T> class MessageFactoryTmpl : public MessageFactory
+template <int ID, typename T>
+class MessageFactoryTmpl : public MessageFactory
 {
 public:
-    int get_msg_id() const override { return ID; }
+  int get_msg_id() const override { return ID; }
 
-    Message* parse(uint8_t const* data, int& size) const override
-    {
-        T* msg = new T();
-        if (!msg->ParseFromArray(data, size)) {
-            // The message cannot be parsed
-            delete msg;
-            return nullptr;
-        }
-        size = msg->ByteSizeLong();
-        return msg;
+  Message* parse(uint8_t const* data, int& size) const override
+  {
+    T* msg = new T();
+    if (!msg->ParseFromArray(data, size)) {
+      // The message cannot be parsed
+      delete msg;
+      return nullptr;
     }
+    size = msg->ByteSizeLong();
+    return msg;
+  }
 };
 
 using MessageFactoryVec = std::vector<MessageFactory const*>;
@@ -60,182 +60,191 @@ int const MSGID_VDFRESULT = 20;
 using MsgFactory_Ping = MessageFactoryTmpl<MSGID_PING, Ping>;
 using MsgFactory_Pong = MessageFactoryTmpl<MSGID_PONG, Pong>;
 using MsgFactory_RequestVDF = MessageFactoryTmpl<MSGID_REQUESTVDF, RequestVDF>;
-using MsgFactory_RequestVDFReply = MessageFactoryTmpl<MSGID_REQUESTVDF_REPLY, RequestVDFReply>;
+using MsgFactory_RequestVDFReply =
+    MessageFactoryTmpl<MSGID_REQUESTVDF_REPLY, RequestVDFReply>;
 using MsgFactory_VDFResult = MessageFactoryTmpl<MSGID_VDFRESULT, VDFResult>;
 
-template <uint16_t MAX_SIZE> class PacketAnalyzer
+template <uint16_t MAX_SIZE>
+class PacketAnalyzer
 {
-    MessageFactoryVec factories_;
-    std::array<uint8_t, MAX_SIZE> data_;
-    int p_ { 0 };
+  MessageFactoryVec factories_;
+  std::array<uint8_t, MAX_SIZE> data_;
+  int p_{0};
 
 public:
-    ~PacketAnalyzer()
-    {
-        for (auto factory : factories_) {
-            delete factory;
-        }
+  ~PacketAnalyzer()
+  {
+    for (auto factory : factories_) {
+      delete factory;
+    }
+  }
+
+  template <typename T>
+  void register_factory()
+  {
+    factories_.push_back(new T);
+  }
+
+  int remaining_bytes() const { return p_; }
+
+  int write(uint8_t const* bytes, int size)
+  {
+    int available = MAX_SIZE - p_;
+    int n = std::min<int>(size, available);
+    memcpy(data_.data() + p_, bytes, n);
+    p_ += n;
+    return n;
+  }
+
+  Message* analyze()
+  {
+    // Analyze the data and create messages
+    if (p_ < 3) {
+      // There are not enough bytes for the message header
+      return nullptr;
     }
 
-    template <typename T> void register_factory() { factories_.push_back(new T); }
-
-    int remaining_bytes() const { return p_; }
-
-    int write(uint8_t const* bytes, int size)
-    {
-        int available = MAX_SIZE - p_;
-        int n = std::min<int>(size, available);
-        memcpy(data_.data() + p_, bytes, n);
-        p_ += n;
-        return n;
+    // First byte is message-id
+    uint8_t msg_id = data_[0];
+    auto i = std::find_if(std::begin(factories_), std::end(factories_),
+                          [msg_id](MessageFactory const* factory) {
+                            return factory->get_msg_id() == msg_id;
+                          });
+    if (i == std::end(factories_)) {
+      return nullptr;
     }
 
-    Message* analyze()
-    {
-        // Analyze the data and create messages
-        if (p_ < 3) {
-            // There are not enough bytes for the message header
-            return nullptr;
-        }
-
-        // First byte is message-id
-        uint8_t msg_id = data_[0];
-        auto i = std::find_if(std::begin(factories_), std::end(factories_),
-            [msg_id](MessageFactory const* factory) { return factory->get_msg_id() == msg_id; });
-        if (i == std::end(factories_)) {
-            return nullptr;
-        }
-
-        // Second word(2-byte) is the size of the message
-        uint16_t size_network;
-        memcpy(&size_network, data_.data() + 1, 2);
-        int size = network_to_host_short(size_network);
-        auto msg = (*i)->parse(data_.data() + 3, size);
-        if (msg == nullptr) {
-            return nullptr;
-        }
-
-        int bytes_to_copy = p_ - size;
-        if (bytes_to_copy > 0) {
-            memcpy(data_.data(), data_.data() + size, bytes_to_copy);
-            p_ -= size;
-        } else {
-            p_ = 0;
-        }
-
-        return msg;
+    // Second word(2-byte) is the size of the message
+    uint16_t size_network;
+    memcpy(&size_network, data_.data() + 1, 2);
+    int size = network_to_host_short(size_network);
+    auto msg = (*i)->parse(data_.data() + 3, size);
+    if (msg == nullptr) {
+      return nullptr;
     }
+
+    int bytes_to_copy = p_ - size;
+    if (bytes_to_copy > 0) {
+      memcpy(data_.data(), data_.data() + size, bytes_to_copy);
+      p_ -= size;
+    } else {
+      p_ = 0;
+    }
+
+    return msg;
+  }
 };
 
 class Session
 {
-    PacketAnalyzer<65535> analyzer_;
+  PacketAnalyzer<65535> analyzer_;
 
-    static int const BUF_SIZE = 4096;
-    tcp::socket sck_;
-    uint8_t read_buf_[BUF_SIZE];
+  static int const BUF_SIZE = 4096;
+  tcp::socket sck_;
+  uint8_t read_buf_[BUF_SIZE];
 
-    void read_next()
-    {
-        sck_.async_read_some(
-            asio::buffer(read_buf_, BUF_SIZE), [this](boost::system::error_code ec, std::size_t bytes) {
-                if (ec) {
-                    // TODO Cannot read from peer
-                    return;
-                }
-                analyzer_.write(read_buf_, bytes);
-                // Analyze message until no message can be found
-                while (1) {
-                    auto msg = analyzer_.analyze();
-                    if (msg == nullptr) {
-                        break;
-                    }
-                    // TODO Notify there is a message has been received
-                }
-            });
-    }
+  void read_next()
+  {
+    sck_.async_read_some(
+        asio::buffer(read_buf_, BUF_SIZE),
+        [this](boost::system::error_code ec, std::size_t bytes) {
+          if (ec) {
+            // TODO Cannot read from peer
+            return;
+          }
+          analyzer_.write(read_buf_, bytes);
+          // Analyze message until no message can be found
+          while (1) {
+            auto msg = analyzer_.analyze();
+            if (msg == nullptr) {
+              break;
+            }
+            // TODO Notify there is a message has been received
+          }
+        });
+  }
 
 public:
-    explicit Session(tcp::socket sck)
-        : sck_(std::move(sck))
-    {
-        analyzer_.register_factory<MsgFactory_Ping>();
-        analyzer_.register_factory<MsgFactory_RequestVDF>();
-    }
+  explicit Session(tcp::socket sck) : sck_(std::move(sck))
+  {
+    analyzer_.register_factory<MsgFactory_Ping>();
+    analyzer_.register_factory<MsgFactory_RequestVDF>();
+  }
 
-    tcp::socket& get_socket() { return sck_; }
+  tcp::socket& get_socket() { return sck_; }
 
-    void run() { }
+  void run() {}
 };
 
 using SessionVec = std::vector<Session>;
 
 class Server
 {
-    asio::io_context& ioc_;
-    tcp::acceptor acceptor_;
-    SessionVec session_vec_;
+  asio::io_context& ioc_;
+  tcp::acceptor acceptor_;
+  SessionVec session_vec_;
 
-    void accept_next_socket()
-    {
-        tcp::socket sck(ioc_);
-        acceptor_.async_accept(sck, [this, sck = std::move(sck)](boost::system::error_code ec) mutable {
-            if (ec) {
-                // TODO Something is wrong
-                return;
-            }
-            Session session(std::move(sck));
-            session.run();
+  void accept_next_socket()
+  {
+    tcp::socket sck(ioc_);
+    acceptor_.async_accept(sck, [this, sck = std::move(sck)](
+                                    boost::system::error_code ec) mutable {
+      if (ec) {
+        // TODO Something is wrong
+        return;
+      }
+      Session session(std::move(sck));
+      session.run();
 
-            session_vec_.push_back(std::move(session));
-        });
-    }
+      session_vec_.push_back(std::move(session));
+    });
+  }
 
 public:
-    Server(boost::asio::io_context& ioc, tcp::endpoint const& endpoint)
-        : ioc_(ioc)
-        , acceptor_(ioc, endpoint)
-    {
-    }
+  Server(boost::asio::io_context& ioc, tcp::endpoint const& endpoint)
+      : ioc_(ioc), acceptor_(ioc, endpoint)
+  {
+  }
 
-    int run()
-    {
-        PLOG_INFO << "exit VDF service.";
-        return 0;
-    }
+  int run()
+  {
+    PLOG_INFO << "exit VDF service.";
+    return 0;
+  }
 };
 
 struct Arguments {
-    std::string listening_addr;
-    unsigned short listening_port;
+  std::string listening_addr;
+  unsigned short listening_port;
 };
 
 int main(int argc, const char* argv[])
 {
-    Arguments args;
-    po::options_description desc;
-    desc.add_options() // All arguments
-        ("help,h", "show help document") // --help
-        ("address", po::value(&args.listening_addr)->default_value("127.0.0.1"),
-            "vdf will listen to this address") // --address
-        ("port", po::value(&args.listening_port)->default_value(9090), "vdf will listen to this port") // --port
-        ;
+  Arguments args;
+  po::options_description desc;
+  desc.add_options()                    // All arguments
+      ("help,h", "show help document")  // --help
+      ("address", po::value(&args.listening_addr)->default_value("127.0.0.1"),
+       "vdf will listen to this address")  // --address
+      ("port", po::value(&args.listening_port)->default_value(9090),
+       "vdf will listen to this port")  // --port
+      ;
 
-    po::variables_map vars;
-    po::store(po::parse_command_line(argc, argv, desc), vars);
-    po::notify(vars);
+  po::variables_map vars;
+  po::store(po::parse_command_line(argc, argv, desc), vars);
+  po::notify(vars);
 
-    if (vars.count("help")) {
-        std::cout << "Usage:" << std::endl;
-        std::cout << desc << std::endl;
-        return 0;
-    }
+  if (vars.count("help")) {
+    std::cout << "Usage:" << std::endl;
+    std::cout << desc << std::endl;
+    return 0;
+  }
 
-    plog::ConsoleAppender<plog::TxtFormatter> console_appender;
-    plog::init(plog::Severity::info, &console_appender);
+  plog::ConsoleAppender<plog::TxtFormatter> console_appender;
+  plog::init(plog::Severity::info, &console_appender);
 
-    asio::io_context ioc;
-    auto addr = address::from_string(args.listening_addr);
-    Server srv(ioc, tcp::endpoint(addr, args.listening_port));
-    return srv.run();
+  asio::io_context ioc;
+  auto addr = address::from_string(args.listening_addr);
+  Server srv(ioc, tcp::endpoint(addr, args.listening_port));
+  return srv.run();
 }
--- src/vdf_test.cpp
+++ src/vdf_test.cpp
@@ -5,54 +5,58 @@
 #include "vdf_computer.h"
 
 // The initial form string is copied from chia-blockchain
-char const* initial_form_str
-    = "0100a27310baa44da5e1357dfd895f260dfa465d16f0f1fbf796e6f2a0df39c5dcbdca2192a6c2def98dc348d3730c416b0b5aaa8a99"
-      "37177b15467694eb5cebac1c1574ae892501a265f62a85c99cfd5b7cb59d31567bfa2e931dc1de8e234da9050100";
+char const* initial_form_str =
+    "0100a27310baa44da5e1357dfd895f260dfa465d16f0f1fbf796e6f2a0df39c5dcbdca2192"
+    "a6c2def98dc348d3730c416b0b5aaa8a99"
+    "37177b15467694eb5cebac1c1574ae892501a265f62a85c99cfd5b7cb59d31567bfa2e931d"
+    "c1de8e234da9050100";
 
 // A challenge infuse to the calculation
-vdf::types::Bytes challenge { 0, 0, 1, 2, 3, 3, 4, 4 };
+vdf::types::Bytes challenge{0, 0, 1, 2, 3, 3, 4, 4};
 
 // the number of iterations we want to run
-uint64_t iters { 10 * 1024 };
+uint64_t iters{10 * 1024};
 
 TEST(VDF, BytesFromStr)
 {
-    auto initial_form = vdf::utils::BytesFromStr(initial_form_str);
-    EXPECT_EQ(initial_form.size(), 100);
-    EXPECT_EQ(initial_form[0], 1);
-    EXPECT_EQ(initial_form[1], 0);
-    EXPECT_EQ(initial_form[2], 0xa2);
-    EXPECT_EQ(initial_form[98], 1);
-    EXPECT_EQ(initial_form[97], 5);
-    EXPECT_EQ(initial_form[96], 0xa9);
+  auto initial_form = vdf::utils::BytesFromStr(initial_form_str);
+  EXPECT_EQ(initial_form.size(), 100);
+  EXPECT_EQ(initial_form[0], 1);
+  EXPECT_EQ(initial_form[1], 0);
+  EXPECT_EQ(initial_form[2], 0xa2);
+  EXPECT_EQ(initial_form[98], 1);
+  EXPECT_EQ(initial_form[97], 5);
+  EXPECT_EQ(initial_form[96], 0xa9);
 }
 
 TEST(VDF, VerifyWithGenesisAndNext)
 {
-    vdf::Computer::InitializeComputer();
-    auto D = vdf::utils::CreateDiscriminant(challenge);
-    vdf::Computer computer(D);
+  vdf::Computer::InitializeComputer();
+  auto D = vdf::utils::CreateDiscriminant(challenge);
+  vdf::Computer computer(D);
 
-    computer.Run(iters);
+  computer.Run(iters);
 
-    vdf::types::Proof proof = computer.GetProof();
-    auto proof_data = vdf::utils::SerializeProof(proof);
-    EXPECT_TRUE(vdf::utils::VerifyProof(D, proof_data, iters));
-    EXPECT_FALSE(vdf::utils::VerifyProof(D, proof_data, iters * 2));
+  vdf::types::Proof proof = computer.GetProof();
+  auto proof_data = vdf::utils::SerializeProof(proof);
+  EXPECT_TRUE(vdf::utils::VerifyProof(D, proof_data, iters));
+  EXPECT_FALSE(vdf::utils::VerifyProof(D, proof_data, iters * 2));
 
-    vdf::Computer computer2(D, proof.y);
+  vdf::Computer computer2(D, proof.y);
 
-    computer2.Run(iters);
+  computer2.Run(iters);
 
-    vdf::types::Proof proof2 = computer2.GetProof();
-    auto proof2_data = vdf::utils::SerializeProof(proof2);
-    EXPECT_TRUE(vdf::utils::VerifyProof(D, proof2_data, iters, proof2.witness_type, proof.y));
+  vdf::types::Proof proof2 = computer2.GetProof();
+  auto proof2_data = vdf::utils::SerializeProof(proof2);
+  EXPECT_TRUE(vdf::utils::VerifyProof(D, proof2_data, iters,
+                                      proof2.witness_type, proof.y));
 
-    vdf::Computer computer3(D, proof2.y);
+  vdf::Computer computer3(D, proof2.y);
 
-    computer3.Run(iters);
+  computer3.Run(iters);
 
-    auto proof3 = computer3.GetProof();
-    auto proof3_data = vdf::utils::SerializeProof(proof3);
-    EXPECT_TRUE(vdf::utils::VerifyProof(D, proof3_data, iters, proof3.witness_type, proof2.y));
+  auto proof3 = computer3.GetProof();
+  auto proof3_data = vdf::utils::SerializeProof(proof3);
+  EXPECT_TRUE(vdf::utils::VerifyProof(D, proof3_data, iters,
+                                      proof3.witness_type, proof2.y));
 }
